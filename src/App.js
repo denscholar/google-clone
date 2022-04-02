9005c40fb360a36f16d05a4d45882dcdbde04485
Dennis Akagha <d.akagha20@gmail.com>
2022-04-02 21:53:43 +0100

    Configure file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar/Navbar';
+import Routes from './components/Routes/Routes';
+import Footer from './components/Footer/Footer';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isDark, setIsDark] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${isDark ? 'dark' : ''}`}>
+      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+        <Navbar/>
+      </div>
     </div>
   );
 }
